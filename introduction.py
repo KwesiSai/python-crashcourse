6c03cf03272f55ad9aed393f8a36b542f151f482
kwesiSai <kwesiabraham4503@gmail.com>
2023-06-07 08:19:29 -0700

    updateintroduction

--- introduction.py
+++ introduction.py
@@ -1,1 +1,5 @@
-print("This is my journey to learning python programming")
+print("This is my journey to python programming")
+#In this journey i am going to learn the basics of python programming, go ahead to study it's application in Data Structures and solve real life issues in OOP.
+print("let's gooooooooo")
+#Not reflecting in Github
+print("Why is my changes not reflecting in Github")
